fddb7b3239311f725e2bab625854b4a41a0a137a
Rodinaaashraf <rodinaashraf515@gmail.com>
2025-05-13 22:40:41 +0300

    listner is done

--- src/components/Listener.py
+++ src/components/Listener.py
@@ -1,3 +1,37 @@
-from ..core import PipelineComponent
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+import time
+from core import PipelineComponent  
+from services import ProcessingStateManager  
+
 class Listener(PipelineComponent):
-        pass
+    def __init__(self, directory, state_manager):
+        self.directory = directory
+        self.state_manager = ProcessingStateManager()
+        self.watching = False
+
+    def process(self, context):
+        return context
+
+    def _get_new_files(self):
+        new_files = []
+        for file_name in os.listdir(self.directory):
+            file_path = os.path.join(self.directory, file_name)
+
+            if os.path.isdir(file_path):
+                continue  
+
+            if not self.state_manager.is_processed(file_path):
+                new_files.append(file_path)
+
+        return new_files
+
+    def start_listening(self, pipeline):
+        self.watching = True
+
+        while self.watching:
+            files = self._get_new_files()
+            for file_path in files:
+                pipeline.process_file(file_path)
+            time.sleep(5)
