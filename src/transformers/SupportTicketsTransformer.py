b1b1f4420d8461123b0cfb991396ac85e3150dd7
Mohamed Hekal <hhekal000@gmail.com>
2025-05-13 19:51:11 +0300

    imp supportTicketTransformer

--- src/transformers/SupportTicketsTransformer.py
+++ src/transformers/SupportTicketsTransformer.py
@@ -1,3 +1,14 @@
 from transformers.loaders import CsvLoader
+from datetime import datetime
 class SupportTicketsTransformer(CsvLoader):
-    pass
+    def transform(self, data):
+        for record in data:
+            # Calculate ticket age
+            complaint_date = datetime.strptime(record['complaint_date'], '%Y-%m-%d')
+            today = datetime.now()
+            record['age'] = (today - complaint_date).days
+            
+            # Add data quality columns
+            self._add_data_quality_columns(record)
+        
+        return data
