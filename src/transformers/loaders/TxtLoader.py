6a310e6b84abd2298a380249c39b250484c119ba
Mohamed Hekal <hhekal000@gmail.com>
2025-05-13 17:08:57 +0300

    update txtLoader

--- src/transformers/loaders/TxtLoader.py
+++ src/transformers/loaders/TxtLoader.py
@@ -3,7 +3,10 @@ class TxtLoader(Transformer):
     def load_data(self, file_path):
         # read the file path as line and return list of lines (as txt)
         """
-        ['customer_id|loan_type|amount_utilized|utilization_date|loan_reason', 'CUST026688|Top-Up Loan|172000|2025-03-02|Let me know if we could spend a lazy Sunday just binge-watching old shows']
+        {'customer_id': 'CUST051615', 'loan_type': 'Loan Against Deposit', 'amount_utilized': '968000', 'utilization_date': '2025-04-12', 'loan_reason': 'Let me know if we could go on a spontaneous road trip without much planning'}
         """
         with open(file_path, 'r') as f:
-            return [line.strip() for line in f if line.strip()]
+            lines = [line.strip() for line in f if line.strip()]
+            headers = lines[0].split('|')  # First line = header
+            data = [dict(zip(headers, line.split('|'))) for line in lines[1:]]
+        return data
